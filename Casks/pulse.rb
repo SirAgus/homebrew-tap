df6f9c48f8a2a405cb85ad19c956e0a1e5c04ce0
SirAgus <afernandez@mq.cl>
2026-01-29 13:05:58 -0300

    update rb

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -2,10 +2,10 @@ cask "pulse" do
   version "0.01.0"
   sha256 "c615f85d065f9e5f86bea5780eb8f565d6d0d26880cc11c72fddbc613db2a8c5"
 
-  url "https://github.com/SirAgus/dynamicIsland/releases/download/v#{version}/PULSE.dmg"
+  url "https://github.com/SirAgus/Pulse/releases/download/v#{version}/PULSE.dmg"
   name "PULSE"
   desc "Dynamic Island for macOS"
-  homepage "https://github.com/SirAgus/dynamicIsland"
+  homepage "https://github.com/SirAgus/Pulse"
 
   app "PULSE.app"
 
