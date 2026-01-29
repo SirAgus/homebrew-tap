c448afd9be8784091cfff64d9099e49e22d8234c
SirAgus <afernandez@mq.cl>
2026-01-29 13:10:15 -0300

    feat: add postflight to remove quarantine attribute from PULSE.app.

--- Casks/pulse.rb
+++ Casks/pulse.rb
@@ -9,6 +9,12 @@ cask "pulse" do
 
   app "PULSE.app"
 
+  postflight do
+    system_command "xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/PULSE.app"],
+                   sudo: false
+  end
+
   zap trash: [
     "~/Library/Application Support/com.agus.pulse",
     "~/Library/Preferences/com.agus.pulse.plist",
